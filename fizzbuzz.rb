b2123fccef163b9ef02f46418d10787f66b303c3
Eric Fiorani <efiorani305@gmail.com>
2018-02-27 15:36:28 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int)
+  if int / 3 == 0
+    puts "Fizz"
+  end
+  elsif int / 5 == 0
+    puts "Buzz"
+  end
+elsif int / 3 == 0 && int / 5 == 0
+  puts "FizzBuzz"
+end
+else nil
+end
+end
+  
